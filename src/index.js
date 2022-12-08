45e473378d3a60d809ec2c769ed80c4e84884dac
samethozman <samet.hozman@outlook.com>
2022-12-08 18:59:00 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 const init = () => {
+    const inputForm = document.querySelector('form');
+    //console.log(form);
   
-}
-
-document.addEventListener('DOMContentLoaded', init);
+    inputForm.addEventListener('submit', function (event) {
+      event.preventDefault();
+      //console.log(e.target['searchByID'].value);
+      //console.log(e.target.children[1].value);
+      //console.log(document.querySelector('#searchByID').value)
+      const input = document.querySelector('input#searchByID');
+  
+      fetch(`http://localhost:3000/movies/${input.value}`)
+      .then(response => response.json())
+      .then(data => {
+        // const movieResult = document.createElement('p');
+        // document.querySelector('h4').appendChild(movieResult);
+        // console.log(data);
+        // movieResult.textContent = (data.title);
+        const title = document.querySelector('section#movieDetails h4');
+        const summary = document.querySelector('section#movieDetails p');
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+      })
+    }) 
+  }
+  
+  document.addEventListener('DOMContentLoaded', init);
